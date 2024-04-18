5407a99b28982db22fe70de106dc53e9272d3cb3
PhuocNghia2002 <truongnghia1209876@gmail.com>
2024-04-18 21:28:44 +0700

    Initial commit

--- web-ecommerce/react/src/pages/HomePage.jsx
+++ web-ecommerce/react/src/pages/HomePage.jsx
@@ -3,8 +3,7 @@ import {Box} from "@mui/material"
 const homepage = () => {
   return (
     <Box>
-      homepage Thấy tui chưa
-      tra loi lai ne
+      homepage
     </Box>
   )
 }
--- web-ecommerce/react/src/routes/router.jsx
+++ web-ecommerce/react/src/routes/router.jsx
@@ -2,19 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import Signin from "../pages/Signin.jsx";
 import Signup from "../pages/Signup.jsx";
 import Homepage from "../pages/HomePage.jsx";
+import GuestLayout from "../components/GuestLayout.jsx";
 
 const router = createBrowserRouter([
   {
-    path: "/signin",
-    element: <Signin />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
     path: "/",
     element: <Homepage />,
+  },
+  {
+    path: "/guest",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "signin",
+        element: <Signin />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+    ],
   },
 ]);
 
