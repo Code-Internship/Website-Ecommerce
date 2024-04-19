3ba5d5e2d183c740e4c1288e56234afe5b86b4b1
PhuocNghia2002 <truongnghia1209876@gmail.com>
2024-04-19 17:11:06 +0700

    update
    page home and sign in , up

--- web-ecommerce/react/src/components/GuestLayout.jsx
+++ web-ecommerce/react/src/components/GuestLayout.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 const GuestLayout = () => {
   return (
-    <div>
-      <Outlet/>
-    </div>
-  )
-}
+    <Box>
+      <Box className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <Box className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+        </Box>
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
 
-export default GuestLayout
+export default GuestLayout;
