6c9bdb87914f6f411888ff384d72d75f25365d47
PhuocNghia2002 <88591278+PhuocNghia2002@users.noreply.github.com>
2024-05-08 17:29:07 +0700

    cap nhat lại

--- web-ecommerce/react/src/pages/ProductDetail.jsx
+++ web-ecommerce/react/src/pages/ProductDetail.jsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import PageComponent from "../components/PageComponent";
+import { Link } from "react-router-dom";
 
 const ProductDetail = () => {
   return (
@@ -8,6 +9,14 @@ const ProductDetail = () => {
         {/** code ben trong nay */}
 
         {/** code ben trong nay */}
+        <Box>
+          <Link
+            to="/"
+            className="font-semibold leading-6 text-dark hover:text-indigo-900 "
+          >
+            Go Home
+          </Link>
+        </Box>
       </PageComponent>
     </Box>
   );
